b649759e4fdbaff772bad1af6af96182fa4e178a
Gilmar Pupo <contact@gpupo.com>
2020-04-01 08:09:18 -0300

    Simplify template

--- src/CsConfigurator.php
+++ src/CsConfigurator.php
@@ -72,16 +72,9 @@ class CsConfigurator
     private $config;
 
     private $template = <<<'EOF'
-This file is part of %s
-Created by %s
-For the information of copyright and license you should read the file
-LICENSE which is distributed with this source code.
-Para a informação dos direitos autorais e de licença você deve ler o arquivo
-LICENSE que é distribuído com este código-fonte.
-Para obtener la información de los derechos de autor y la licencia debe leer
-el archivo LICENSE que se distribuye con el código fuente.
-For more information, see <%s>.
-
+This file is part of %s created by %s
+For the information of copyright and license you should read the file LICENSE which is
+distributed with this source code. For more information, see <%s>
 EOF;
 
     public function __construct($directory, Finder $finder = null, $rules = null, $usingCache = true)
